6b640bd3d7d7a5463b1ac3326df262ddc3862588
Ezekiel Newren <ezekielnewren@gmail.com>
2025-04-12 10:31:25 -0600

    generate 440Hz sine wave for 2 seconds

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::f32::consts::PI;
+use std::path::PathBuf;
+
+fn main() {
+    let dir_dump = dirs::home_dir().unwrap().join("tmp");
+
+
+    let sample_rate = 44100;
+    let duration_secs = 2;
+    let freq = 440.0;
+    let amp = 0.5;
+
+
+    let total_samples = sample_rate * duration_secs;
+
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: sample_rate as u32,
+        bits_per_sample: 8,
+        sample_format: hound::SampleFormat::Int,
+    };
+
+    let mut writer = hound::WavWriter::create(dir_dump.join("tmp.wav"), spec).unwrap();
+
+    for n in 0..total_samples {
+        let t = n as f32 / sample_rate as f32;
+        let sample = amp * (2.0 * PI * freq * t).sin();
+
+        let int_samp = (sample * i8::MAX as f32) as i8;
+
+        writer.write_sample(int_samp).unwrap();
+    }
+
+    writer.finalize().unwrap();
+
+    println!("{}", dir_dump.as_path().display());
+
+}
