cfef45d8c9e1a580e347b60263be6ac2c00a4976
Ezekiel Newren <ezekielnewren@gmail.com>
2025-04-13 13:24:56 -0600

    fsk modulate "hello world"

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::f32::consts::PI;
-use std::path::PathBuf;
-
+use bitvec::prelude::*;
 
 struct Tone {
     freq: f32,
@@ -12,14 +11,11 @@ fn main() {
     let dir_dump = dirs::home_dir().unwrap().join("tmp");
 
 
-    let sample_rate = 44100;
-    let duration_secs = 2;
+    let sample_rate = 44100.0;
+    let sps = 0.1;
 
-    let mut mixer = Vec::new();
-    mixer.push(Tone { freq: 440.0, amp: 0.9 });
-    mixer.push(Tone { freq: 660.0, amp: 0.5 });
-
-    let total_samples = sample_rate * duration_secs;
+    let lo = Tone { freq: 440.0, amp: 1.0 };
+    let hi = Tone { freq: 660.0, amp: 1.0 };
 
     let spec = hound::WavSpec {
         channels: 1,
@@ -30,16 +26,30 @@ fn main() {
 
     let mut writer = hound::WavWriter::create(dir_dump.join("tmp.wav"), spec).unwrap();
 
-    for n in 0..total_samples {
-        let t = n as f32 / sample_rate as f32;
-        let mut sample = 0.0;
-        for v in mixer.iter() {
-            sample += v.amp * (2.0 * PI * v.freq * t).sin();
-        }
-        sample /= mixer.len() as f32;
+    let message = "hello world";
+    let bytes = message.as_bytes();
 
-        let int_samp = (sample * i8::MAX as f32) as i8;
-        writer.write_sample(int_samp).unwrap();
+    let mut bitstream = bitvec![0; 0];
+    for b in bytes {
+        bitstream.push(((*b >> 0) & 1) != 0);
+        bitstream.push(((*b >> 1) & 1) != 0);
+        bitstream.push(((*b >> 2) & 1) != 0);
+        bitstream.push(((*b >> 3) & 1) != 0);
+        bitstream.push(((*b >> 4) & 1) != 0);
+        bitstream.push(((*b >> 5) & 1) != 0);
+        bitstream.push(((*b >> 6) & 1) != 0);
+        bitstream.push(((*b >> 7) & 1) != 0);
+    }
+
+    for bit in bitstream {
+        let tone = if bit { &hi } else { &lo };
+        let mut tick = 0.0f32;
+        for _ in 0..(sps * sample_rate) as usize {
+            let sample = tone.amp * (2.0 * PI * tone.freq * (tick / sample_rate)).sin();
+            let int_samp = (sample * i8::MAX as f32) as i8;
+            writer.write_sample(int_samp).unwrap();
+            tick += 1.0;
+        }
     }
 
     writer.finalize().unwrap();
